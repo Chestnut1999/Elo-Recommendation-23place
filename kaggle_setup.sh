855216b68b8d7397a133c509a965f7c12a83a823
Ubuntu <ubuntu@ip-10-0-0-253.ap-northeast-1.compute.internal>
2018-11-28 13:51:18 +0900

    some

---
+++ kaggle_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p py input output feim
+mkdir -p features/1_first_valid features/2_second_valid features/3_third_valid 
+mkdir features/4_winner
+mkdir features/5_tmp
+mkdir features/9_delete
+mkdir valid
+mkdir submit
+mkdir stack
+mkdir eda
+touch output/py_train.py.log
