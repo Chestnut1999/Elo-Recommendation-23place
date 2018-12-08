bed9cc8352abc8da38ca6cd95fca0b1e314ef3ee
Ubuntu <ubuntu@ip-10-0-0-253.ap-northeast-1.compute.internal>
2018-12-08 11:11:19 +0900

    some

--- py/bq_feature_to_pkl.py
+++ py/bq_feature_to_pkl.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import os
 HOME = os.path.expanduser("~")
@@ -7,14 +8,14 @@ import utils
 import glob
 import re
 
-path_list = glob.glob('../features/bigquery/feat903*auth_0*')
 
-feat_no = '903_au0_'
+path_list = glob.glob(f'../features/bigquery/*905*auth_1*')
 
 for path in path_list:
 
     #  fname = 'his_' + re.search(r'his_([^/.]*).csv', path).group(1)
-    #  fname = re.search(r'his_([^/.]*).csv', path).group(1)
+    fname = re.search(r'feat([^/.]*)_auth', path).group(1)
+    feat_no = f"{fname}_au1_"
     df = pd.read_csv(path)
 
     base = utils.read_df_pkl('../input/base0*')
@@ -24,7 +25,12 @@ for path in path_list:
 
     for col in df.columns:
         if col.count('__'):
-            utils.to_pkl_gzip(path=f"../features/1_first_valid/{feat_no}train_{col.replace('__', '@').replace('his_', '') }",
+            utils.to_pkl_gzip(path=f"../features/1_first_valid/{feat_no}train_{col.replace('__', '@').replace('his_', '')}@",
                               obj=train[col].values)
-            utils.to_pkl_gzip(path=f"../features/1_first_valid/{feat_no}test_{col.replace('__', '@').replace('his_', '')}",
+            utils.to_pkl_gzip(path=f"../features/1_first_valid/{feat_no}test_{col.replace('__', '@').replace('his_', '')}@",
+                              obj=test[col].values)
+        else:
+            utils.to_pkl_gzip(path=f"../features/1_first_valid/{feat_no}train_{col}@",
+                              obj=train[col].values)
+            utils.to_pkl_gzip(path=f"../features/1_first_valid/{feat_no}test_{col}@",
                               obj=test[col].values)
