0ad568042f4c6073ca79c3e8b25035073879196d
Ubuntu <ubuntu@ip-10-0-0-253.ap-northeast-1.compute.internal>
2018-12-09 10:47:56 +0900

    some

--- py/start.sh
+++ py/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-python s102_lgb_decrease.py
-python s102_lgb_decrease.py
-python s102_lgb_decrease.py
+for i in `seq 1 2000`
+do
+    echo $i
+    python s104_lgb_ensemble.py lgb 0.05 $i
+done
 
-wait
-sudo shutdown -P
+# wait
+# sudo shutdown -P
