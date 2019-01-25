c860a38edba718c2472ad54af2dec07c1ca6d5f4
Ubuntu <ubuntu@ip-10-0-0-253.ap-northeast-1.compute.internal>
2019-01-25 15:33:37 +0900

    some

--- py/s001_lgb_main.py
+++ py/s001_lgb_main.py
@@ -1,4 +1,5 @@
-out_part = ['', 'part', 'all'][0]
+out_part = ['', 'part', 'all'][2]
+#  num_threads = 32
 num_leaves = 31
 #  num_leaves = 16
 #  num_leaves = 48
@@ -60,29 +61,16 @@ params = params_elo()[1]
 params['learning_rate'] = learning_rate
 
 # Best outlier fit LB3.690
-#  params['max_depth'] = 7
-#  params['colsample_bytree'] = 0.6
-#  num_boost_round = 100000
 #  num_leaves = 4
-#  params['colsample_bytree'] = 0.2
 params['num_leaves'] = num_leaves
-#  params['max_depth'] = 5
-#  params['subsample'] = 0.9
-#  params['colsample_bytree'] = 0.35
-#  params['min_child_samples'] = 50
 if num_leaves>40:
     params['num_leaves'] = num_leaves
     params['subsample'] = 0.8757099996397999
     params['colsample_bytree'] = 0.7401342964627846
     params['min_child_samples'] = 61
 
-#  params['boosting'] = 'dart'
-#  params['drop_rate'] = 0.05
-#  params['max_drop'] = 5
-
 
 start_time = "{0:%Y%m%d_%H%M%S}".format(datetime.datetime.now())
-
 
 #========================================================================
 # Data Load
@@ -92,12 +80,16 @@ try:
     if int(sys.argv[2])>0:
         win_path = f'../model/2017{sys.argv[2]}/4_winner/*.gz'
         tmp_path_list = glob.glob('../model/2017{sys.argv[2]}/5_tmp/*.gz')
-    else:
     # features配下のwinnerを使う時
+    else:
         win_path = f'../features/4_winner/*.gz'
         tmp_path_list = glob.glob('../features/5_tmp/*.gz')
+        # features配下のセットを使いつつ、first_monthを絞りたい場合
         if int(sys.argv[2])<0:
-            sys.argv[2] = int(sys.argv[2])*-1
+            if int(sys.argv[2])<10:
+                sys.argv[2] = '0' + str(int(sys.argv[2])*-1)
+            else:
+                sys.argv[2] = str(int(sys.argv[2])*-1)
 except ValueError:
     # ALLのwinnerを使う時
     if sys.argv[2]=='all':
@@ -115,17 +107,17 @@ except ValueError:
             tmp_path_list = glob.glob(f'../features/exp/*.gz')
 
 
-base = utils.read_df_pkl('../input/base*')
-win_path_list = glob.glob(win_path) + tmp_path_list
-
-base = utils.read_df_pkl('../input/base*')
+base = utils.read_df_pkl('../input/base_first*')
 base_train = base[~base[target].isnull()].reset_index(drop=True)
 base_test = base[base[target].isnull()].reset_index(drop=True)
+
+win_path_list = glob.glob(win_path) + tmp_path_list
 feature_list = utils.parallel_load_data(path_list=win_path_list)
 df_feat = pd.concat(feature_list, axis=1)
 train = pd.concat([base_train, df_feat.iloc[:len(base_train), :]], axis=1)
 test = pd.concat([base_test, df_feat.iloc[len(base_train):, :].reset_index(drop=True)], axis=1)
-y = train[target].values
+# スードラベリングの時はtrainとtestのconcatした長さになる
+pl_length = 0
 
 #========================================================================
 # card_id list by first active month
@@ -135,19 +127,33 @@ try:
         test_latest_id_list = np.load(f'../input/card_id_test_first_active_2017{sys.argv[2][:2]}.npy')
 
         if sys.argv[2][-2:]=='pl':
-            pred_path = glob.glob(f'../model/2017{sys.argv[2][:2]}/stack/*')[0]
+            pred_path = glob.glob(f'../model/2017{sys.argv[2][:2]}/stack/*org0_*')[0]
             pred_col = 'pred'
             pred_feat = utils.read_pkl_gzip(pred_path)
             train[pred_col] = pred_feat[:len(train)]
             train.loc[~train[key].isin(train_latest_id_list), target] = train.loc[~train[key].isin(train_latest_id_list), pred_col]
+
             tmp_test = test.copy()
             tmp_test[target] = pred_feat[len(train):]
-            train = pd.concat([train, tmp_test], axis=0).drop(pred_col, axis=1)
+
+            # first_active_monthが201712より前の場合、学習データセットから未来のfirst_active_monthを除外する
+            if int(sys.argv[2][:2])<12:
+                base = base[base['first_active_month'] <= f'2017-{sys.argv[2][:2]}']
+                train = train.merge(base[key].to_frame(), how='inner', on=key)
+                test = test.merge(base[key].to_frame(), how='inner', on=key)
+                tmp_test = tmp_test.merge(base[key].to_frame(), how='inner', on=key)
+
+
+            train = pd.concat([train, tmp_test], axis=0, ignore_index=True).drop(pred_col, axis=1)
+            pl_length = len(train)
             del tmp_test
             gc.collect()
         else:
-            train = train.loc[train[key].isin(train_latest_id_list), :].reset_index(drop=True)
-            test = test.loc[test[key].isin(test_latest_id_list), :].reset_index(drop=True)
+            #  train = train.loc[train[key].isin(train_latest_id_list), :].reset_index(drop=True)
+            #  test = test.loc[test[key].isin(test_latest_id_list), :].reset_index(drop=True)
+            #  base = base[base['first_active_month'] <= f'2017-{sys.argv[2][:2]}']
+            #  train = train.merge(base[key].to_frame(), how='inner', on=key)
+            #  test = test.merge(base[key].to_frame(), how='inner', on=key)
             submit = []
 except IndexError:
     pass
@@ -163,6 +169,15 @@ except TypeError:
     #  submit = []
 #========================================================================
 
+if 'first_active_month' in list(train.columns):
+    train.drop('first_active_month', axis=1, inplace=True)
+    test.drop('first_active_month', axis=1, inplace=True)
+y = train[target].values
+
+# Target Check
+#  y = np.round(y, 0)
+#  print(pd.Series(y).value_counts())
+#  sys.exit()
 
 #========================================================================
 # FM STACK
@@ -189,7 +204,7 @@ if out_part=='part':
     #  tmp2 = train[train.prediction<0.01][train.target>-30]
     tmp1 = train[train.pred_mean>0.01]
     tmp2 = train[train.pred_mean<0.01][train.target>-30]
-    train = pd.concat([tmp1, tmp2], axis=0)
+    train = pd.concat([tmp1, tmp2], axis=0, ignore_index=True)
     del tmp1, tmp2
     gc.collect()
     #  train.drop('prediction', axis=1, inplace=True)
@@ -244,11 +259,19 @@ for i, seed in enumerate(seed_list):
     #      params['colsample_bytree'] = 0.7401342964627846
     #      params['min_child_samples'] = 61
 
-    if len(train)>150000:
-        train['outliers'] = train[target].map(lambda x: 1 if x<-30 else 0)
+    if len(train)>150000 and pl_length==0:
+        train['outliers'] = train[target].map(lambda x: 1 if x<-3 else 0)
         folds = StratifiedKFold(n_splits=fold, shuffle=True, random_state=seed)
         kfold = folds.split(train,train['outliers'].values)
         train.drop('outliers', axis=1, inplace=True)
+
+    elif pl_length > 0 or int(sys.argv[2][:2]) != 0:
+        # Psued Labelingだと、outlierと予測された値は-7 ~ -18あたりになりそう?
+        train['outliers'] = train[target].map(lambda x: 1 if x<=-3 else 0)
+        folds = StratifiedKFold(n_splits=fold, shuffle=True, random_state=seed)
+        kfold = folds.split(train,train['outliers'].values)
+        train.drop('outliers', axis=1, inplace=True)
+
     else:
         kfold = False
         fold_type = 'kfold'
@@ -278,6 +301,9 @@ for i, seed in enumerate(seed_list):
 
     seed_pred += LGBM.prediction
 
+    if pl_length>0:
+        LGBM.result_stack = LGBM.result_stack.reset_index(drop=True).iloc[:pl_length,:]
+
     if i==0:
         cv_list.append(LGBM.cv_score)
         iter_list.append(LGBM.iter_avg)
@@ -293,6 +319,7 @@ for i, seed in enumerate(seed_list):
         df_pred = df_pred.merge(LGBM.result_stack[[key, 'prediction']].rename(columns={'prediction':f'prediction_{i}'}), how='inner', on=key)
 
     try:
+        # first month modelで全体に対する予測値を出してstackする特徴を保存する場合
         sys.argv[4]
         model_list += LGBM.fold_model_list
     except IndexError:
@@ -311,24 +338,30 @@ if len(stack_name)>0:
 
 
 #========================================================================
-# エキスパートモデルを作成した際、全体に対する予測値を出してStackingできるようにする
+# First Month Pred For Stack
+# expert modelを作成した際、全体に対する予測値を出してStackingできるようにする
 try:
     sys.argv[4]
     use_cols = LGBM.use_cols
 
     if sys.argv[2][-2:]=='pl':
-        pred = df_pred['pred_mean'].values
+        if int(sys.argv[2][:2])<12:
+            df_pred = base.merge(df_pred.drop(target, axis=1), how='inner', on=key)
+            pred = df_pred['pred_mean'].values
+        else:
+            pred = df_pred['pred_mean'].values
         y_train = y
+
     else:
         # Reload Dataset
-        base = utils.read_df_pkl('../input/base*')
+        base = utils.read_df_pkl('../input/base_first0*')
         base_train = base[~base[target].isnull()].reset_index(drop=True)
         base_test = base[base[target].isnull()].reset_index(drop=True)
         feature_list = utils.parallel_load_data(path_list=win_path_list)
         df_feat = pd.concat(feature_list, axis=1)
         train = pd.concat([base_train, df_feat.iloc[:len(base_train), :]], axis=1)
         test = pd.concat([base_test, df_feat.iloc[len(base_train):, :].reset_index(drop=True)], axis=1)
-        train_test = pd.concat([train, test], axis=0)[use_cols]
+        train_test = pd.concat([train, test], axis=0, ignore_index=True)[use_cols]
         y_train = train[target].values
 
         pred = np.zeros(len(train_test))
@@ -337,9 +370,9 @@ try:
         pred /= len(model_list)
 
     y_pred = pred[:len(y_train)]
-    score = np.sqrt(mean_squared_error(y_train, y_pred))
+    score = np.sqrt(mean_squared_error(np.where(y_train!=y_train, 0, y_train), y_pred))
 
-    utils.to_pkl_gzip(obj=pred, path=f'../model/2017{sys.argv[2][:2]}/stack/{start_time[4:13]}_elo_first_month2017{sys.argv[2]}_{len(seed_list)}seed_lr{learning_rate}_round{num_boost_round}_CV{str(score)[:6]}')
+    utils.to_pkl_gzip(obj=pred, path=f"../model/2017{sys.argv[2][:2]}/stack/{start_time[4:13]}_elo_first_month2017{sys.argv[2]}_{len(seed_list)}seed_lr{str(learning_rate).replace('.', '-')}_round{num_boost_round}_CV{str(score)[:6].replace('.', '-')}")
     #  pd.Series(LGBM.use_cols, name='use_cols').to_csv( f'../model/2017{sys.argv[2][:2]}/stack/{start_time[4:8]}_elo_first_month2017{sys.argv[2]}_fold_model_use_cols.csv',  index=False)
 except IndexError:
     pass
@@ -359,35 +392,24 @@ logger.info(f'''
 #========================================================================
 # Part of card_id Score
 try:
-    if int(sys.argv[2][:2])>0:
-        if len(train)>150000:
-            for i in range(201701, 201713, 1):
-                train_latest_id_list = np.load(f'../input/card_id_train_first_active_{i}.npy')
-
-                part_train = df_pred.loc[df_pred[key].isin(train_latest_id_list), :]
-                y_train = part_train[target].values
-                if 'pred_mean' in list(part_train.columns):
-                    y_pred = part_train['pred_mean'].values
-                else:
-                    y_pred = part_train['prediction'].values
-                part_score = np.sqrt(mean_squared_error(y_train, y_pred))
-                logger.info(f'''
-                #========================================================================
-                # First Month {i} of Score: {part_score} | N: {len(train_latest_id_list)}
-                #========================================================================''')
-except ValueError:
-    if len(train)>150000:
+    if pl_length>0 or sys.argv[2]=='all' or len(train)>150000:
         for i in range(201701, 201713, 1):
             train_latest_id_list = np.load(f'../input/card_id_train_first_active_{i}.npy')
 
             part_train = df_pred.loc[df_pred[key].isin(train_latest_id_list), :]
             y_train = part_train[target].values
-            y_pred = part_train['prediction'].values
+            if 'pred_mean' in list(part_train.columns):
+                y_pred = part_train['pred_mean'].values
+            else:
+                y_pred = part_train['prediction'].values
             part_score = np.sqrt(mean_squared_error(y_train, y_pred))
+
             logger.info(f'''
             #========================================================================
             # First Month {i} of Score: {part_score} | N: {len(train_latest_id_list)}
             #========================================================================''')
+except ValueError:
+    pass
 except TypeError:
     pass
 #========================================================================
@@ -415,8 +437,10 @@ except IndexError:
 
 # Save
 try:
-    if int(sys.argv[2][:2])==0:
+    if int(sys.argv[2])==0:
         utils.to_pkl_gzip(path=f"../stack/{start_time[4:12]}_stack_{model_type}_lr{learning_rate}_{feature_num}feats_{len(seed_list)}seed_{num_leaves}leaves_iter{iter_avg}_OUT{str(out_score)[:7]}_CV{str(cv_score).replace('.', '-')}_LB", obj=df_pred)
+except ValueError:
+    pass
 except TypeError:
     pass
 
@@ -430,46 +454,53 @@ cv_feim.to_csv( f'../valid/{start_time[4:12]}_valid_{model_type}_lr{learning_rat
 #========================================================================
 # Submission
 try:
-    if int(sys.argv[2][:2])==0:
+    if int(sys.argv[2])==0:
         test_pred = seed_pred / len(seed_list)
         submit[target] = test_pred
         submit_path = f'../submit/{start_time[4:12]}_submit_{model_type}_lr{learning_rate}_{feature_num}feats_{len(seed_list)}seed_{num_leaves}leaves_iter{iter_avg}_OUT{str(out_score)[:7]}_CV{cv_score}_LB.csv'
         submit.to_csv(submit_path, index=False)
+except ValueError:
+    pass
 except TypeError:
-    sys.exit()
+    pass
 #========================================================================
 
 #========================================================================
 # CV INFO
 
-if int(sys.argv[2][:2])==0 and len(train)>150000:
+try:
+    if int(sys.argv[2])==0 and len(train)>150000:
 
-    import re
-    path_list = glob.glob('../log_submit/01*CV*LB*.csv')
-    path_list.append(submit_path)
-    #  path_list_2 = glob.glob('../check_submit/*.csv')
-    #  path_list += path_list_2
+        import re
+        path_list = glob.glob('../log_submit/01*CV*LB*.csv')
+        path_list.append(submit_path)
+        #  path_list_2 = glob.glob('../check_submit/*.csv')
+        #  path_list += path_list_2
 
-    tmp_list = []
-    path_list = list(set(path_list))
-    for path in path_list:
-        tmp = pd.read_csv(path)
-        tmp_path = path.replace(".", '-')
-        cv = re.search(r'CV([^/.]*)_LB', tmp_path).group(1).replace('-', '.')
-        lb = re.search(r'LB([^/.]*).csv', tmp_path).group(1).replace('-', '.')
-        #  if lb<'3.690' and path!=submit_path:
-        #      continue
-        tmp.rename(columns={'target':f"CV{cv[:9]}_LB{lb}"}, inplace=True)
-        tmp.set_index('card_id', inplace=True)
-        tmp_list.append(tmp.copy())
+        tmp_list = []
+        path_list = list(set(path_list))
+        for path in path_list:
+            tmp = pd.read_csv(path)
+            tmp_path = path.replace(".", '-')
+            cv = re.search(r'CV([^/.]*)_LB', tmp_path).group(1).replace('-', '.')
+            lb = re.search(r'LB([^/.]*).csv', tmp_path).group(1).replace('-', '.')
+            #  if lb<'3.690' and path!=submit_path:
+            #      continue
+            tmp.rename(columns={'target':f"CV{cv[:9]}_LB{lb}"}, inplace=True)
+            tmp.set_index('card_id', inplace=True)
+            tmp_list.append(tmp.copy())
 
-    if len(tmp_list)>0:
-        df = pd.concat(tmp_list, axis=1)
-        df_corr = df.corr(method='pearson')
+        if len(tmp_list)>0:
+            df = pd.concat(tmp_list, axis=1)
+            df_corr = df.corr(method='pearson')
 
-        logger.info(f'''
+            logger.info(f'''
 #========================================================================
 # OUTLIER FIT SCORE: {out_score}
 # SUBMIT CORRELATION:
 {df_corr[f'CV{str(cv_score)[:9]}_LB'].sort_values()}
 #========================================================================''')
+except ValueError:
+    pass
+except TypeError:
+    pass
