c1b2f0380df3990527bc8069857f5cb93d4d2f97
Ubuntu <ubuntu@ip-10-0-0-253.ap-northeast-1.compute.internal>
2019-02-07 15:47:13 +0900

    some

--- py/start.sh
+++ py/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 python s001_lgb_main.py 0.01 0 10 ods
-python s001_lgb_main.py 0.01 0 10 ods
-python s001_lgb_main.py 0.01 0 10 ods
+python s003_lgb_params_tune.py
 
 # python s019_arange_dist_train.py 45 2016-08 5 pmo
 
